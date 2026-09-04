9415c20b6be2b3c2015ebb4e2081c93b0ccf89af
pal <juanpalomo0229@gmail.com>
2026-09-04 17:12:36 -0500

    p

--- 5to_punto.cpp
+++ 5to_punto.cpp
@@ -13,40 +13,31 @@ Point calcularCentroide(Point puntos[], int n) {
     if (n <= 0) {
         return centroide;
     }
-
     double sumaX = 0.0;
     double sumaY = 0.0;
-
     for (int i = 0; i < n; i++) {
         sumaX += puntos[i].x;
         sumaY += puntos[i].y;
     }
-
     centroide.x = sumaX / n;
     centroide.y = sumaY / n;
-
     return centroide;
 }
-
 int main() {
     int n;
-
     cout << "Ingrese la cantidad de puntos: ";
     cin >> n;
-
     if (n <= 0) {
         cout << "La cantidad de puntos debe ser mayor a 0." << endl;
         return 1;
     }
     Point puntos[n];
-
     for (int i = 0; i < n; i++) {
         cout << "Ingrese las coordenadas (x y) del punto " << (i + 1) << ": ";
         cin >> puntos[i].x >> puntos[i].y;
     }
     Point centroide = calcularCentroide(puntos, n);
-
-    cout << "\n--- Resultado ---" << endl;
+    cout << "Resultado" << endl;
     cout << "El centroide es: (" << centroide.x << ", " << centroide.y << ")" << endl;
     return 0;
 }
